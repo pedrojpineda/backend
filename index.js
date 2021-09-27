ee7a290e74190230fe61754f8f1653c292b45098
Pedro Pineda <pedrojosepineda@hotmail.com>
2021-09-26 20:55:47 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const URL = 'mongodb://localhost:27017/ecommerce'
-async function CRUD() {
-    await mongoose.connect('mongodb://localhost:27017/test');
-}
